d57da57aa7b53d38b302f0cf8f4c740b768814eb
pbedyn <pawel.bedynski@gmail.com>
2019-05-09 11:01:19 +0200

    Step 2 - create list of tracks from circles

--- create_list_tracks.py
+++ create_list_tracks.py
@@ -174,27 +174,46 @@ def create_list_links(path2data, longitude, latitude):
 # execution of the mian part of the program #################################
 if __name__ == "__main__":
 
-    # set paths
-    path2data, path2new_batch, path2drivers, path2tracks_file = set_paths()
-    
-    non_scraped_file = "Non_scraped.txt"
-    non_scraped_file_path = path2data + non_scraped_file
+#    # set paths
+#    path2data, path2new_batch, path2drivers, path2tracks_file = set_paths()
+#    
+#    non_scraped_file = "Non_scraped.txt"
+#    non_scraped_file_path = path2data + non_scraped_file
+    path2circles = "D:\\flights\\Circles\\East\\"
     
     L_tracks = []
     L_points = []
     L_dates = []
     
-    for longitude in range(13, 15, 2):
-        for latitude in range(51, 55, 2):
-            new_L_tracks, new_L_points, new_L_dates = create_list_links(path2data + "Circles\\", longitude, latitude)
-            L_tracks = L_tracks + new_L_tracks
-            L_points = L_points + new_L_points
-            L_dates = L_dates + new_L_dates
-            print(longitude, latitude)
-    
+    for longitude in range(1, 27, 2):
+        for latitude in range(35, 59, 2):
+            try:
+                new_L_tracks, new_L_points, new_L_dates = create_list_links(path2circles, longitude, latitude)
+                L_tracks = L_tracks + new_L_tracks
+                L_points = L_points + new_L_points
+                L_dates = L_dates + new_L_dates
+                print(longitude, latitude)
+            except:
+                pass
     
     df_tracks = pd.DataFrame()
-    df_tracks['tracks'] = L_tracks
+    df_tracks['track'] = L_tracks
     df_tracks['points'] = L_points
-    df_tracks['dates'] = L_dates
+    df_tracks['date'] = L_dates
     df_tracks['scraped'] = 0
+    df_tracks = df_tracks.drop_duplicates(keep = 'first')
+    
+    
+    # merge with the xc_tracks.csv database and update column 'scraped'
+    xc_tracks = pd.read_csv("D:\\flights\\xc_tracks.csv", sep = ',')
+    # rename "Unnamed: 0" column
+    xc_tracks = xc_tracks.rename(columns={'Unnamed: 0': 'id'})
+    # merge with df_tracks
+    df_tracks = pd.merge(df_tracks, xc_tracks, on = 'track', how = 'left')
+    df_tracks['scraped'][df_tracks['id'] < 25000] = 1
+    df_tracks = df_tracks.drop(['id'], axis = 1)
+    df_tracks = df_tracks[pd.to_numeric(df_tracks['points'], errors='coerce').notnull()]
+    df_tracks['points'] = df_tracks['points'].astype(float)
+    df_tracks = df_tracks.sort_values(by = ['points'], ascending = False)
+    df_tracks.to_csv("D:\\flights\\df_tracks.csv")
+    del new_L_tracks, new_L_points, new_L_dates, L_tracks, L_points, L_dates, xc_tracks
